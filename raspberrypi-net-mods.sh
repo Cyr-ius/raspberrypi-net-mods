23139c25859651f021e6b75f4ada054473b5d483
Cyr-ius <cyr-ius@ipocus.net>
2017-07-21 18:57:57 +0200

    Fix wpa_supplicant for Debian Stretch

---
+++ raspberrypi-net-mods.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Part of raspi-config https://github.com/RPi-Distro/raspi-config
+#
+# See LICENSE file for copyright and license details
+
+VERSION_ID=$(cat /etc/os-realse | grep VERSION_ID)
+if [ $VERSION_ID=8 ];then
+/bin/mv /boot/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+/bin/chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
+fi
+
+if [ $VERSION_ID=9 ];then
+/bin/mv /boot/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+/bin/chmod 600 /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+systemctl enable wpa_supplicant@wlan0.service
+fi
